c9931211c0e0f10844f26c1fb052eba4ff173faa
BrunelliJazmin405318 <405318@tecnicatura.frc.utn.edu.ar>
2025-10-07 13:00:31 -0300

    HU: Marcar pieza como irreparable completa y probada

--- src/main/java/ar/edu/utn/tfi/web/AdminOrdenController.java
+++ src/main/java/ar/edu/utn/tfi/web/AdminOrdenController.java
@@ -1,6 +1,7 @@
 package ar.edu.utn.tfi.web;
 import ar.edu.utn.tfi.repository.OrdenTrabajoRepository;
 import ar.edu.utn.tfi.service.OrderAdvanceService;
+import ar.edu.utn.tfi.service.OrderIrreparableService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -13,10 +14,12 @@ import java.util.Map;
 public class AdminOrdenController {
     private final OrdenTrabajoRepository ordenRepo;
     private final OrderAdvanceService service;
+    private final OrderIrreparableService irreparableService;
 
-    public AdminOrdenController(OrdenTrabajoRepository ordenRepo, OrderAdvanceService service) {
+    public AdminOrdenController(OrdenTrabajoRepository ordenRepo, OrderAdvanceService service,  OrderIrreparableService irreparableService) {
         this.ordenRepo = ordenRepo;
         this.service = service;
+        this.irreparableService = irreparableService;
     }
 
     @PostMapping("/{nro}/avanzar")
@@ -26,5 +29,13 @@ public class AdminOrdenController {
                 .orElseThrow(() -> new EntityNotFoundException("Orden no encontrada: " + nro));
         service.avanzarEtapa(orden.getId(), auth.getName());
         return ResponseEntity.ok(Map.of("message","Etapa avanzada correctamente", "nroOrden", nro));
+    }
+    @PutMapping("/{nro}/pieza-irreparable")
+    public ResponseEntity<?> marcarIrreparable(@PathVariable String nro, Authentication auth) {
+        irreparableService.marcarIrreparablePorNro(nro, auth.getName());
+        return ResponseEntity.ok(Map.of(
+                "message", "Orden marcada como PIEZA_IRREPARABLE",
+                "nroOrden", nro
+        ));
     }
 }
